c112a8c1cd09411dff9ef0a579efb2d8577bed8c
a.sumin <a.sumin@cardsmobile.ru>
2020-04-09 10:32:28 +0300

    hometask_3: Приведение к необходимой структуре

---
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/Context.kt
@@ -0,0 +1,4 @@
+package ru.skillbranch.skillarticles.extensions
+
+class Context {
+}
--- app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
@@ -1,12 +1,13 @@
-package ru.skillbranch.skillarticles
+package ru.skillbranch.skillarticles.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.skillbranch.skillarticles.R
 
-class MainActivity : AppCompatActivity() {
+class RootActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_root)
     }
 }
